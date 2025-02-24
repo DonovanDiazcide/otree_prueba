c4c28eeabd5581e41b251e0a622e7721581a0b13
DonovanDiazcide <donobyron20@gmail.com>
2025-02-24 12:41:44 -0600

    los amo

--- iat/__init__.py
+++ iat/__init__.py
@@ -15,7 +15,7 @@ Implicit Association Test, draft
 """
 from statistics import mean, stdev
 
-
+print("los amo")
 def dscore1(data3: list, data4: list, data6: list, data7: list):
     # Filtrar valores demasiado largos.
     def not_long(value):
